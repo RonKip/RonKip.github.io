0ec4650dc84d0660c6603bc77cab5368b2c3d8fa
Ankushdeep <ankush5200@gmail.com>
2024-08-26 12:01:15 -0400

    remove unwanted loop

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function show(ind){
     var arr = ["Candy", "Tethreemino", "Train", "Cuttoo"]
 
-    for (var i = 0 ; i < arr.length; i++){
-        var url = "./" + arr[i] + "/" + "index.html";
-
-        window.open(url);
-    }
+    var url = "./" + arr[ind] + "/" + "index.html";
+    window.open(url);
+    
 }
