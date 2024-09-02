b60705d73b44f9622078e87b9ec582d27c1e962b
Ankushdeep <ankush5200@gmail.com>
2024-09-02 04:31:33 -0400

    new game Towall

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function show(ind){
-    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo"]
+    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall"]
 
     var url = "./" + arr[ind-1] + "/" + "index.html";
     window.open(url);
