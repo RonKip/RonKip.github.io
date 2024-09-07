006facba848d9367ec3a76845617c753f3dce62f
Ronald Bett <ankush5200@gmail.com>
2024-09-07 10:59:54 -0400

    new game eneza

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function show(ind){
-    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall"]
+    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall", "Eneza"]
 
     var url = "./" + arr[ind-1] + "/" + "index.html";
     window.open(url);
