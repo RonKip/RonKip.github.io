f1454508b99c67d50d25a1407f8e4fb521b68d69
Ronald Bett <ankush5200@gmail.com>
2024-09-15 07:40:55 -0400

    new game ziggotopia

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function show(ind){
-    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall", "Eneza", "Cornered", "Cardle"]
+    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall", "Eneza", "Cornered", "Cardle", "Ziggo"]
 
     var url = "./" + arr[ind-1] + "/" + "index.html";
     window.open(url);
