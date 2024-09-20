c566eb7037042e75a330762abbebcd8facd82db0
Ronald Bett <ankush5200@gmail.com>
2024-09-20 01:54:35 -0400

    new game waal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function show(ind){
-    var arr = ["Candy", "Tethreemino", "Train", "Cuttoo", "Towall", "Eneza", "Cornered", "Cardle", "Ziggo"]
+    var arr = [
+        "Candy", "Tethreemino", "Train", "Cuttoo", "Towall", 
+        "Eneza", "Cornered", "Cardle", "Ziggo", "Waal"]
 
     var url = "./" + arr[ind-1] + "/" + "index.html";
     window.open(url);
